30b78a9d0a0a280b474216c15daea5e5b97c4490
Roman Havrylko <groman2000@gmail.com>
2025-12-13 22:32:57 +0200

    feat: added userRoutes and messageRoutes to routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import healthRoutes from './health.routes.js';
 
+import messageRoutes from '../modules/messages/message.routes.js';
+import userRoutes from '../modules/users/user.routes.js';
+
 const router = Router();
 
 router.use(healthRoutes);
+router.use(userRoutes);
+router.use(messageRoutes);
 
 export default router;
