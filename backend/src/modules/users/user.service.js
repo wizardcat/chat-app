c82cb719331d1cddd91582bced4df09a21160e6b
Roman Havrylko <groman2000@gmail.com>
2025-12-13 22:32:08 +0200

    feat: added findUserBySocketId and updateLastSeenAt to user service

--- backend/src/modules/users/user.service.js
+++ backend/src/modules/users/user.service.js
@@ -8,8 +8,16 @@ export class UserService {
     return await this.userModel.create({ nickname, socketId });
   }
 
-  async deleteUser(socketId) {
+  async findUserBySocketId(socketId) {
+    return await this.userModel.findOne({ where: { socketId } });
+  }
+
+  async deleteUserBySocketId(socketId) {
     return await this.userModel.destroy({ where: { socketId } });
+  }
+
+  async updateLastSeenAt(socketId) {
+    return await this.userModel.update({ lastSeenAt: new Date() }, { where: { socketId } });
   }
 
   async getOnlineUsersCount() {
