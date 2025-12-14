be92e7a88b29f7b562e7774fd72567e64ddea063
Roman Havrylko <groman2000@gmail.com>
2025-12-14 13:33:24 +0200

    feat: created ChatApp component

---
+++ frontend/components/ChatApp/chat-app.component.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { Box } from '@mui/material';
+
+const boxStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+};
+
+export const ChatApp = () => {
+  return <Box sx={boxStyles} ></Box>;
+};
