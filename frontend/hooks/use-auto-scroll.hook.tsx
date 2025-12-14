41fd84f72e065622f06d4043386f9981cd6f38db
Roman Havrylko <groman2000@gmail.com>
2025-12-14 20:50:24 +0200

    feat: added useAutoScroll hook

---
+++ frontend/hooks/use-auto-scroll.hook.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'react';
+
+export const useAutoScroll = (dependency: unknown[]) => {
+  const endRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [dependency]);
+
+  return endRef;
+};
