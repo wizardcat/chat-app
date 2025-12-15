1866d1e81fc67ec961d5fd5450c01881d64194b6
Roman Havrylko <groman2000@gmail.com>
2025-12-15 18:44:31 +0200

    style: applyed theme to message list

--- frontend/components/MessageList/message-list.component.tsx
+++ frontend/components/MessageList/message-list.component.tsx
@@ -4,31 +4,34 @@ import React from 'react';
 import { MessageItem } from '../MessageItem/message-item.component';
 import { MessageListProps } from './message-list.props';
 
-export const MessageList = React.memo(({ messages, currentNickname, isLoading }: MessageListProps) => {
-  const messagesEndRef = useAutoScroll([messages]);
+export const MessageList = React.memo(
+  ({ messages, currentNickname, isLoading }: MessageListProps) => {
+    const messagesEndRef = useAutoScroll([messages]);
 
-  return (
-    <Box flex={1} overflow="auto" p={2} bgcolor="grey.100">
-      <Container maxWidth="lg">
-        {isLoading ? (
-          <Box display="flex" justifyContent="center" py={4}>
-            <CircularProgress />
-          </Box>
-        ) : messages.length === 0 ? (
-          <Box textAlign="center" py={6}>
-            <Typography variant="h6" color="text.secondary">
-              No messages yet. Start the conversation!
-            </Typography>
-          </Box>
-        ) : (
-          messages.map((msg) => (
-            <MessageItem key={msg.id} message={msg} isOwn={msg.nickname === currentNickname} />
-          ))
-        )}
-        <div ref={messagesEndRef} />
-      </Container>
-    </Box>
-  );
-});
+    return (
+      <Box flex={1} overflow="auto" bgcolor="background.default" py={2}>
+        <Container maxWidth="lg">
+          {isLoading ? (
+            <Box display="flex" justifyContent="center" py={6}>
+              <CircularProgress />
+            </Box>
+          ) : messages.length === 0 ? (
+            <Box textAlign="center" py={6}>
+              <Typography variant="body1" color="text.secondary">
+                No messages yet. Start the conversation!
+              </Typography>
+            </Box>
+          ) : (
+            messages.map((msg) => (
+              <MessageItem key={msg.id} message={msg} isOwn={msg.nickname === currentNickname} />
+            ))
+          )}
+
+          <div ref={messagesEndRef} />
+        </Container>
+      </Box>
+    );
+  },
+);
 
 MessageList.displayName = 'MessageList';
