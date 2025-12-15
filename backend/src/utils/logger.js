ae17c30516f3232c6461cd7b25e863726f6a9e44
Roman Havrylko <groman2000@gmail.com>
2025-12-15 19:24:27 +0200

    feat: added logger

---
+++ backend/src/utils/logger.js
@@ -0,0 +1,17 @@
+import pino from 'pino';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+export const logger = pino({
+  level: process.env.LOG_LEVEL || (isProd ? 'info' : 'debug'),
+  transport: !isProd
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+          ignore: 'pid,hostname',
+        },
+      }
+    : undefined,
+});
