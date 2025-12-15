cf1ba70c65587d6b29525d29b1095fd36baefa23
Roman Havrylko <groman2000@gmail.com>
2025-12-15 18:44:10 +0200

    style: applyed theme to message item

--- frontend/components/MessageItem/message-item.component.tsx
+++ frontend/components/MessageItem/message-item.component.tsx
@@ -1,9 +1,21 @@
 import { MAX_MESSAGE_WIDTH } from '@/config/api.config';
 import { formatTimestamp } from '@/utils/format-timestamp';
 import { getAvatarUrl } from '@/utils/get-avatar-url';
-import { Avatar, Box, Paper, Typography } from '@mui/material';
+import { Avatar, Box, Paper, Theme, Typography } from '@mui/material';
 import React from 'react';
 import { MessageItemProps } from './message-item.props';
+
+const avatarStyles = {
+  width: 32,
+  height: 32,
+  mr: 1,
+};
+
+const paperStyles = (isOwn: boolean) => (theme: Theme) => ({
+  p: 1.5,
+  bgcolor: isOwn ? theme.palette.success.light : theme.palette.common.white,
+  borderRadius: 2,
+});
 
 export const MessageItem = React.memo(({ message, isOwn }: MessageItemProps) => {
   const avatarUrl = getAvatarUrl(message.nickname);
@@ -12,8 +24,7 @@ export const MessageItem = React.memo(({ message, isOwn }: MessageItemProps) => 
   return (
     <Box display="flex" justifyContent={isOwn ? 'flex-end' : 'flex-start'} mb={2}>
       <Box display="flex" alignItems="flex-start" maxWidth={MAX_MESSAGE_WIDTH}>
-        {!isOwn && <Avatar src={avatarUrl} alt={message.nickname} sx={{ mr: 1 }} />}
-
+        {!isOwn && <Avatar src={avatarUrl} alt={message.nickname} sx={avatarStyles} />}
         <Box>
           <Box display="flex" alignItems="center" mb={0.5}>
             <Typography variant="caption" color="text.secondary" fontWeight="bold" mr={1}>
@@ -24,21 +35,13 @@ export const MessageItem = React.memo(({ message, isOwn }: MessageItemProps) => 
             </Typography>
           </Box>
 
-          <Paper
-            sx={{
-              p: 1.5,
-              bgcolor: isOwn ? 'primary.main' : 'white',
-              color: isOwn ? 'white' : 'text.primary',
-              borderRadius: 2,
-            }}
-          >
+          <Paper sx={paperStyles(isOwn)}>
             <Typography variant="body1" sx={{ wordBreak: 'break-word' }}>
               {message.message}
             </Typography>
           </Paper>
         </Box>
-
-        {isOwn && <Avatar src={avatarUrl} alt={message.nickname} sx={{ ml: 1 }} />}
+        {isOwn && <Avatar src={avatarUrl} alt={message.nickname} sx={avatarStyles} />}
       </Box>
     </Box>
   );
