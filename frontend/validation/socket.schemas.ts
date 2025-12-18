b85fa89ea56ecbdce60594329e2dbe475dd3b815
Roman Havrylko <groman2000@gmail.com>
2025-12-18 21:14:49 +0200

    feat: added schemas

---
+++ frontend/validation/socket.schemas.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const joinSchema = z
+  .string()
+  .trim()
+  .min(2, 'Nickname too short')
+  .max(30, 'Nickname too long');
+
+export const sendMessageSchema = z.object({
+  message: z.string().trim().min(1, 'Message too short').max(500, 'Message too long'),
+});
