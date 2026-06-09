6d55012ae3c7d606f872c64b6020dce4b81d3056
Roman Havrylko <groman2000@gmail.com>
2026-06-09 09:09:47 +0300

    feat: added emotion cache

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import { chatTheme } from '@/theme/chat-theme';
+import { CacheProvider } from '@emotion/react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import createEmotionCache from './emotion-cache';
+
+const clientSideEmotionCache = createEmotionCache();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider theme={chatTheme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <CacheProvider value={clientSideEmotionCache}>
+      <ThemeProvider theme={chatTheme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
